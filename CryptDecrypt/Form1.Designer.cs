a5df794b0f13768ec58ac5df2ddd5decc594bc7e
Mohssine EL HARFI <m.elharfi@gmail.com>
2016-01-26 14:40:22 +0000

    upload

---
+++ CryptDecrypt/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CryptDecrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ext = new System.Windows.Forms.TextBox();
+            this.crypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.copy = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.indice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(5, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fichiers sources à Crypter :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Parcourir ...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // info
+            // 
+            this.info.BackColor = System.Drawing.Color.Black;
+            this.info.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.info.Location = new System.Drawing.Point(10, 90);
+            this.info.Name = "info";
+            this.info.ReadOnly = true;
+            this.info.Size = new System.Drawing.Size(475, 163);
+            this.info.TabIndex = 4;
+            this.info.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(5, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nouvelle extension :";
+            // 
+            // ext
+            // 
+            this.ext.Location = new System.Drawing.Point(135, 33);
+            this.ext.Name = "ext";
+            this.ext.Size = new System.Drawing.Size(38, 20);
+            this.ext.TabIndex = 6;
+            this.ext.Text = "dat";
+            // 
+            // crypt
+            // 
+            this.crypt.Location = new System.Drawing.Point(270, 32);
+            this.crypt.Name = "crypt";
+            this.crypt.Size = new System.Drawing.Size(102, 23);
+            this.crypt.TabIndex = 7;
+            this.crypt.Text = "Crypter";
+            this.crypt.UseVisualStyleBackColor = true;
+            this.crypt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(380, 32);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(103, 23);
+            this.decrypt.TabIndex = 8;
+            this.decrypt.Text = "Decrypter";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // copy
+            // 
+            this.copy.AutoSize = true;
+            this.copy.Location = new System.Drawing.Point(186, 36);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(53, 17);
+            this.copy.TabIndex = 9;
+            this.copy.Text = "Copie";
+            this.copy.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "By Mohssine EL HARFI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(4, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Indice de décalage :";
+            // 
+            // indice
+            // 
+            this.indice.Location = new System.Drawing.Point(135, 59);
+            this.indice.MaxLength = 3;
+            this.indice.Name = "indice";
+            this.indice.Size = new System.Drawing.Size(31, 20);
+            this.indice.TabIndex = 12;
+            this.indice.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Brown;
+            this.label5.Location = new System.Drawing.Point(170, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(319, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Indice dois être supérrieur ou égale à 0 et inférrieur ou égale à 255";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(495, 277);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.indice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.copy);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.crypt);
+            this.Controls.Add(this.ext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Crypt Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox info;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ext;
+        private System.Windows.Forms.Button crypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.CheckBox copy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox indice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ CryptDecrypt/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CryptDecrypt
+{
+    public partial class Form1 : Form
+    {
+        List<string> files;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                files = openFileDialog1.FileNames.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // crypter le fichier
+            button1.Enabled = false;
+            crypt.Enabled = false;
+            decrypt.Enabled = false;
+
+            for (int cnt = 0; cnt < files.Count; cnt++)
+            {
+                if (files[cnt].Split('.').Count() == 2)
+                {
+                    if (ext.Text != "")
+                    {
+                        if (indice.Text == "")
+                        {
+                            MessageBox.Show("Veuillez remplire l'indice bouffon");
+                            indice.Focus();
+                            return;
+                        }
+
+                        byte _indice;
+                        bool result = byte.TryParse(indice.Text, out _indice);
+                        if (!result)
+                        {
+                            MessageBox.Show("Indice dois être numérique bouffon");
+                            indice.Text = "";
+                            indice.Focus();
+                            return;
+                        }
+                        else if (result && (_indice < 0 || _indice > 255))
+                        {
+                            MessageBox.Show("L'indice dois être superrieur ou égale 0 et inférieur ou égale à 255,\nC'est pourtant écris dessus BOUFFON");
+                            indice.Text = "";
+                            indice.Focus();
+                            return;
+                        }
+
+                        info.Text += "\rChemain complet : " + files[cnt];
+
+                        byte[] b1 = File.ReadAllBytes(files[cnt]);
+                        info.Text += "\nAnalyse de fichier en cours ...";
+
+                        if (b1.Count() > 4 && b1[b1.Count() - 3] == 48 && b1[b1.Count() - 2] == 48 && b1[b1.Count() - 1] == 57)
+                            MessageBox.Show("Ce fichier est déja crypté");
+                        else
+                        {
+                            info.Text += "\nFichier non crypté";
+                            if (b1.Count() > 0)
+                            {
+                                if (copy.Checked)
+                                {
+                                    try
+                                    {
+                                        File.Copy(files[cnt], files[cnt] + ".bak");
+                                        info.Text += "\nBack up de fichier a été fait sous le nom :\n" + files[cnt] + ".bak";
+                                    }
+                                    catch
+                                    {
+                                        File.Delete(files[cnt] + ".bak");
+                                        File.Copy(files[cnt], files[cnt] + ".bak");
+                                        info.Text += "\nUn autre fichier de backup a été trouvé mais a été écrasé";
+                                    }
+                                }
+                                else
+                                    info.Text += "\nLe fichier source sera écrasé.";
+                                info.Text += "\nLe fichier contiens " + b1.Count().ToString() + " Bytes";
+                                for (int cnt2 = 0; cnt2 < b1.Length; cnt2++)
+                                    b1[cnt2] += _indice;
+                                byte[] b2 = new byte[b1.Count() + 4];
+                                b1.CopyTo(b2, 0);
+                                b2[b2.Count() - 4] = (byte)(48 + _indice);        // byte de 0 = 49 mais on ajoute +1 pour que ca egale 48 lors du decryptage qui soustrait -1
+                                b2[b2.Count() - 3] = (byte)(48 + _indice);
+                                b2[b2.Count() - 2] = (byte)(57 + _indice);
+                                string oldExt = files[cnt].Substring(files[cnt].IndexOf('.') + 1, files[cnt].Length - files[cnt].IndexOf('.') - 1);
+                                char firstCharExt = oldExt.ElementAt(0);
+                                byte b4 = Convert.ToByte(firstCharExt);
+                                byte b3 = Convert.ToByte(oldExt.ElementAt(0) + _indice);
+                                b2[b2.Count() - 1] = Convert.ToByte(oldExt.ElementAt(0) + _indice);
+                                File.WriteAllBytes(files[cnt], b2);
+                                info.Text += "\nFichier Crypté avec succée";
+                                string newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + "." + ext.Text;
+                                try
+                                {
+                                    File.Move(files[cnt], newPath);
+                                }
+                                catch (Exception ex)
+                                {
+                                    File.Delete(newPath);
+                                    File.Move(files[cnt], newPath);
+                                    info.Text += "\nUn autre fichier .Dat a été trouvé mais a été écrasé";
+                                }
+
+                                info.Text += "\nFichier rennomé en :" + newPath;
+                                files[cnt] = "";
+                            }
+                            else
+                                MessageBox.Show("Le fichier source est vide");
+
+                            info.Text += System.Environment.NewLine;
+                        }
+                    }
+                    else
+                        MessageBox.Show("Vous n'avez pas precisé d'extension");
+                }
+                else
+                    MessageBox.Show("Le fichier source ne dois pas contenir plus d'un point '.'");
+            }
+            MessageBox.Show(files.Count + " fichiers ont étés cryptés", "Succée", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            button1.Enabled = true;
+            crypt.Enabled = true;
+            decrypt.Enabled = true;
+
+            info.SelectionStart = info.Text.Length - 1;
+            info.SelectionLength = 0;
+            info.ScrollToCaret();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // decrypter le fichier
+            button1.Enabled = false;
+            crypt.Enabled = false;
+            decrypt.Enabled = false;
+
+            for (int cnt = 0; cnt < files.Count; cnt++)
+            {
+                if (indice.Text == "")
+                {
+                    MessageBox.Show("Veuillez remplire l'indice bouffon");
+                    indice.Focus();
+                    return;
+                }
+
+                byte _indice;
+                bool result = byte.TryParse(indice.Text, out _indice);
+                if (!result)
+                {
+                    MessageBox.Show("Indice dois être numérique bouffon");
+                    indice.Text = "";
+                    indice.Focus();
+                    return;
+                }
+                else if (result && (_indice < 0 || _indice > 255))
+                {
+                    MessageBox.Show("L'indice dois être superrieur ou égale 0 et inférieur ou égale à 255,\nC'est pourtant écris à coté BOUFFON");
+                    indice.Text = "";
+                    indice.Focus();
+                    return;
+                }
+
+                info.Text += "\rChemain complet : " + files[cnt];
+
+                byte[] b1 = File.ReadAllBytes(files[cnt]);
+                if (b1.Count() > 0)
+                {
+                    if (b1.Count() > 4)
+                    {
+                        info.Text += "\nLe fichier contiens " + b1.Count().ToString() + " Bytes";
+                        for (int cnt2 = 0; cnt2 < b1.Length; cnt2++)
+                            b1[cnt2] -= _indice;
+
+                        info.Text += "\nAnalyse du fichier";
+                        if (b1.Count() > 4 && b1[b1.Count() - 4] == 48 && b1[b1.Count() - 3] == 48 && b1[b1.Count() - 2] == 57)
+                        {
+                            if (copy.Checked)
+                            {
+                                try
+                                {
+                                    File.Copy(files[cnt], files[cnt] + ".bak");
+                                    info.Text += "\nBack up de fichier a été fait sous le nom :\n" + files[cnt] + ".bak";
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show("Impossible de faire un Backup, sois un autre fichier existe déja qui porte le nom :\n" + files[cnt] + ".bak\nOu espace disque insufisant.\n\n" + ex.ToString());
+                                    Close();
+                                }
+                            }
+                            else
+                                info.Text += "\nLe fichier source sera écrasé.";
+
+                            info.Text += "\nFichier Crypté.\nDécryptage en cours...";
+
+                            byte[] b2 = new byte[b1.Count() - 4];
+                            for (int cnt2 = 0; cnt2 < b2.Count(); cnt2++)
+                                b2[cnt2] = b1[cnt2];
+                            File.WriteAllBytes(files[cnt], b2);
+                            string newPath;
+                            if (b1.Last() == 116)
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".txt";
+                            else if (b1.Last() == 109)
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".mp3";
+                            else if (b1.Last() == 106)
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".jpg";
+                            else if (b1.Last() == 112)
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".png";
+                            else if (b1.Last() == 103)
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".gif";
+                            else
+                            {
+                                newPath = files[cnt].Substring(0, files[cnt].IndexOf('.')) + ".dat";
+                                info.Text += "\nExtension d'origine inconnue, Byte[" + b2.Last() + "] Ascii[" + Convert.ToChar(b1.Last()) + "]";
+                            }
+
+                            try
+                            {
+                                File.Move(files[cnt], newPath);
+                                info.Text += "\nFichier rennomé en :" + newPath;
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("\nImpossible de renommé le fichier :\n" + files[cnt] + "\nen:\n" + newPath + "\n\n" + ex.ToString(), "Error Writing file");
+                                Close();
+                            }
+
+                            info.Text += "\nFichier Décrypté avec succée.";
+                            files[cnt] = "";
+                            if (b1.Count() == 4)
+                                info.Text += "\nFichier décrypté mais vide,Code = 3 Bytes + 1 Byte extension";
+
+                            info.Text += System.Environment.NewLine;
+                        }
+                        else
+                            MessageBox.Show("Ce fichier n'est pas crypté, ou endommagé");
+                    }
+                    else
+                        MessageBox.Show("Ce fichier n'est pas crypté, ou endommagé");
+                }
+                else
+                    MessageBox.Show("Le fichier source est vide");    
+            }
+
+            MessageBox.Show(files.Count + " fichiers ont étés décryptés", "Succée", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            button1.Enabled = true;
+            crypt.Enabled = true;
+            decrypt.Enabled = true;
+
+            info.SelectionStart = info.Text.Length - 1;
+            info.SelectionLength = 0;
+            info.ScrollToCaret();
+        }
+    }
+}
